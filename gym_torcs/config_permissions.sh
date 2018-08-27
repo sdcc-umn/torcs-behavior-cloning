951fbdbee9d4854a8b8a4fb3bfb4ed658f9abbd3
njbittner <njbittner@gmail.com>
2018-08-27 08:42:45 -0700

    adding permissions script

---
+++ gym_torcs/config_permissions.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+chmod 0666 /usr/local/share/games/torcs/config/graph.xml
+chmod 0666 /usr/local/share/games/torcs/config/raceman/practice.xml
+chmod 0666 /usr/local/share/games/torcs/config/screen.xml
